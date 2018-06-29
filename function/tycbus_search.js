f55f41ba68c55bea8a3de257a6f6d8f8188b95f1
DESKTOP-IG5BHNH\USER <ewen06263@gmail.com>
2018-06-29 17:08:36 +0800

    1.完成公車API (以sync-request實現)

--- function/tycbus_search.js
+++ function/tycbus_search.js
@@ -3,11 +3,10 @@ var fs = require("fs");
 var busurl = 'http://apidata.tycg.gov.tw/OPD-io/bus4/GetEstimateTime.xml?routeIds=';
 var busdata = {};
 var tybus = require('./tybus.json');
-var rp = require('request-promise');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 
-var name = "1"
+//var name = "1"
 
 function BusStation(name){ //以路線之名稱取得路線之ID
 	var data = tybus.BusDynInfo.BusInfo.Route
@@ -22,72 +21,20 @@ function BusStation(name){ //以路線之名稱取得路線之ID
 }
 //console.log(BusStation(name))
 
-
-
-/* function getBusData (name){
+exports.getBusData = function(name){
+	var request = require('sync-request');
 	busurl = busurl + BusStation(name);
-	http.get(busurl, function(response){
-		var data = '';
-			// response event 'data' 當 data 陸續接收的時候，用一個變數累加它。
-		response.on('data', function(chunk){
-			data += chunk;
+	var res = request('GET', busurl);
+	var BusData ;
+	if(res.statusCode == 200){
+		//console.log(res.getBody('utf-8'));
+		parser.parseString(res.getBody('utf-8'), function (err, result) {
+			//console.log(result.BusDynInfo.BusInfo[0].Route[0].EstimateTime);
+			BusData = result.BusDynInfo.BusInfo[0].Route[0].EstimateTime; 
 		});
-			// response event 'end' 當接收 data 結束的時候。
-		response.on('end', function(){
-			//console.log(data);
-			parser.parseString(data, function (err, result) {
-				return result.BusDynInfo.BusInfo[0].Route[0].EstimateTime
-			});
-		});
-	}).on('error', function(e){ // http get 錯誤時
-		  console.log("error: ", e);
-	});
-		
+		return BusData
+	}else{
+		return '擷取錯誤'
+	}
 }
-console.log(getBusData(name))*/
-
-/*function getBusData (name){
-	busurl = busurl + BusStation(name);
-rp(busurl).promise().bind(this)
-    .then(function (data) {
-		parser.parseString(data, function (err, result) {
-			console.log(result.BusDynInfo.BusInfo[0].Route[0].EstimateTime)
-			return result.BusDynInfo.BusInfo[0].Route[0].EstimateTime
-		});      
-    })
-    .catch(function (err) {
-        // Crawling failed...
-		console.log('failed')
-    });
-console.log("123");
-};
-console.log(getBusData(name))*/
-
-function test(){
-	let test = 'I am test';
-	let request = require('request');
-	// sync
-	new Promise((resolve, reject) => {
-		request({
-			url: 'https://github.com',
-			method: 'get'
-		}, (err, res, body) => {
-			if (res && res.statusCode === 200) {
-				resolve(res.statusCode + ' ok!');
-				console.log('123')
-			} else {
-				reject(' error - -');
-			}
-		});
-	}).then(result => {
-		test = result;
-		
-		console.log("outside request: " + test);
-		return test
-	}).catch(err => {
-		console.log("error: " + err)
-	})
-}
-console.log('321');
-console.log(test());
-
+//console.log(getBusData(name))
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app     = express();
 var config = require('./config/config.js');
 const routes = require('./routes');
 var ubike = require('./function/ubike_search.js');
+var tycbus = require('./function/tycbus_search.js');
 var train = require('./function/train_search.js');
 var router = express.Router();// 建立 Router 物件
 
@@ -12,6 +13,7 @@ routes(app) //抓取路由設定
 app.listen(config.config.port,function(){
 	console.log("伺服器已啟動 Port : " + config.config.port);
 	ubike.getubikedata();
-	console.log(train.getTrainTime("臺北","高雄"));
+	console.log(tycbus.getBusData('1'));
+	//console.log(train.getTrainTime("臺北","高雄"));
 });
 
