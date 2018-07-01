8790c5176b637e871b98946ced3d567ce7872d14
DESKTOP-IG5BHNH\USER <ewen06263@gmail.com>
2018-07-01 16:35:12 +0800

    細節BUG修正(定時GET自己、Call 系統變數PORT)

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const config = {
-	"port" : 8080
+	"port" : 8080 || process.env.PORT
 };
 exports.config = config;
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var router = express.Router();// 建立 Router 物件
 routes(app) //抓取路由設定
 
 // ---- 啟動伺服器 ----
-app.listen(config.config.port||process.env.PORT,function(){
+app.listen(Number(config.config.port),function(){
 	console.log("伺服器已啟動 Port : " + config.config.port);
 	ubike.getubikedata() //擷取ubikeData
 	//console.log(tycbus.getBusData('1'));
@@ -25,3 +25,24 @@ app.listen(config.config.port||process.env.PORT,function(){
 setInterval(function(){
 	ubike.getubikedata() //擷取ubikeData
 },60000)
+
+setInterval(function(){
+	
+	var http = require("http");
+	var url='http://twtrafficinfo.herokuapp.com/';
+	http.get(url, function(response){
+    var data1 = '';
+
+    response.on('data1', function(chunk){
+        data1 += chunk;
+    });
+       response.on('end', function(){
+      
+        data1 = JSON.parse(data1);      
+    });
+}).on('error', function(e){ // http get 錯誤時
+      console.log("error: ", e);
+	  
+});	
+	console.log('GET成功');		
+},1080000);
